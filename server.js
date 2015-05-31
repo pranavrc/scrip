0e3042f89fb16fe3c130d1044b8e9c7f599f8e72
Pranav Ravichandran <me@onloop.net>
2015-05-31 20:04:27 +0530

    Return 422 for invalid parameters

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var parser = require('./src/scrip').parse;
 
 app.get('/', function (req, res) {
   var scrip = req.query.scrip;
-  var result = parser(scrip);
-
-  res.send(result);
+  if (scrip) {
+    var result = parser(scrip);
+    res.status(200).json(result);
+  } else {
+    res.status(422).json({'error': 'Invalid data'});
+  }
 });
 
 app.listen('8000');
 
+module.exports = app;
